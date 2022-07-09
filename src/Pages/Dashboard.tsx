6786f4bebd045ae2b04a691cc2d5c3141664138a
Krisna31 <krisnaaaputraaa@gmail.com>
2022-07-09 18:06:29 +0700

    Official done by me -krisna31 created : 3 days next : if have time do refactoring, maintance, remove bug endif

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -4,9 +4,15 @@ import Authorize from "../molecules/Authorize";
 
 
 export default function Dashboard({ getUserData }: { getUserData: UserDataType }) {
+  let userData = JSON.parse(localStorage.getItem("datauser") || '{}');
+  if (!userData.isLoggedIn) localStorage.setItem("datauser", JSON.stringify(getUserData))
+  userData = JSON.parse(localStorage.getItem("datauser") || '{}');
+
+  getUserData = userData;
+
   return (
     <>
-      {getUserData.isLoggedIn ? <Authorize getUserData={getUserData} /> : <Navigate to="/" replace />}
+      {getUserData.isLoggedIn || userData.isLoggedIn ? <Authorize getUserData={getUserData} /> : <Navigate to="/" replace />}
     </>
   );
 }
--- src/atoms/AlertYesOrNo.tsx
+++ src/atoms/AlertYesOrNo.tsx
@@ -5,7 +5,7 @@ import { deleteNote } from "../utils/firebase/CRUD";
 function AlertYesOrNo({ setIsShowAlert, dataDeleteNote }: { setIsShowAlert: Dispatch<SetStateAction<boolean>>, dataDeleteNote: DataDeleteNoteType | undefined }) {
   return (
     <div className="fixed bg-black w-screen h-screen top-0 bottom-0 right-0 left-0 slateTransparent flex justify-center items-center">
-      <div className="bg-slate-50 p-10 flex flex-col w-1/2 shadow-xl md:w-1/4 rounded-xl dark:bg-slate-300">
+      <div className="bg-slate-50 p-10 flex flex-col w-1/2 shadow-xl md:w-2/5 rounded-xl dark:bg-slate-300">
         <h1 className="text-red-600 text-center font-bold">Are You Sure Delete This Data?</h1>
         <button className='bg-red-600 rounded-lg my-5 p-3 text-white hover:bg-red-800' onClick={() => {
           if (!(dataDeleteNote === undefined)) deleteNote(dataDeleteNote.e, dataDeleteNote.note, dataDeleteNote.userID, dataDeleteNote.getNotes, dataDeleteNote.setNotes)
